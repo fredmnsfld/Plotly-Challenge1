603b3b060d1adea009c271f87b9696b49cf396b3
fredmnsfld <fred.mnsfld@gmail.com>
2020-11-19 16:19:10 -0800

    Add option to index

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,3 +2,11 @@
 d3.json('samples.json').then(data => {
     console.log(data);
 })
+
+// event handler select drop down
+let dropdown = d3.select('#selDatasets')
+console.log(dropdown)
+
+dropdown.on('change', function(){
+    console.log(d3.event.target.value)
+})
